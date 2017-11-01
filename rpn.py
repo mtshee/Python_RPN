78189413b16d8f9af2ca378a1fe81d4a95abd866
mtshee <mtshee@umich.edu>
2017-11-01 14:30:09 -0400

    First Commit

---
+++ rpn.py
@@ -0,0 +1,31 @@
+#!/user/bin/env python3
+import operator
+
+ops = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul
+}
+def calculate(string):
+	stack = []
+	for token in string.split():
+		try:
+			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = ops[token]
+			result = function(arg1, arg2)
+			stack.append(result)
+	#print(stack)
+	return stack.pop()
+	
+	
+def main():
+	while True:
+		calculate(input("RPN calc> "))
+
+if __name__ == '__main__':
+	main()
+
+
